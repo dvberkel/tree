072e380ded3195835108764860ebf5c29abc0d1f
Daan van Berkel <daan.van.berkel@alliander.com>
2020-01-03 18:40:00 +0100

    expand an lindemayer system

--- docs/js/sketch.js
+++ docs/js/sketch.js
@@ -5,3 +5,47 @@ function setup() {
 function draw() {
     ellipse(mouseX, mouseY, 80, 80);
 }
+
+var tree_system = {
+    start: 'S',
+    rules: {
+        'S': [[1, 'FF'],
+              [2, 'G']],
+        'F': [[1, 'FF']],
+        'G': [[1, 'GF']]
+    }
+}
+
+function expand(system, n) {
+    var word = system.start;
+    for (var index = 0; index < n; index++) {
+        word = substitute(system.rules, word);
+    }
+    return word;
+}
+
+function substitute(rules, word) {
+    var result = '';
+    for (var index = 0; index < word.length; index++) {
+        var symbol = word[index];
+        if (rules[symbol]) {
+            result += select_one(rules[symbol]);
+        } else {
+            result += symbol;
+        }
+    }
+    return result;
+}
+
+function select_one(options) {
+    var total_weight = options.map(function (option) { return option[0]; }).reduce(function (sum, v) { return sum + v; });
+    var target = total_weight * Math.random();
+    var accumulated_weight = 0;
+    for (var index = 0; index < options.length; index++) {
+        accumulated_weight += options[index][0];
+        if (accumulated_weight > target) {
+            return options[index][1];
+        }
+    }
+    return options[options.length - 1][1];
+}
